03b1ddb86d38760a8098de69012ac3f66455f796
claireyuan92 <meng.yuan@duke.edu>
2015-03-22 02:40:05 -0400

    edited main.c

--- main.c
+++ main.c
@@ -2,17 +2,30 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #define THRES 5
-  
+
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 
 
+void send_alert_to_phone(){
+  DictionaryIterator *iter;
 
-void select_multi_click_handler(ClickRecognizerRef recognizer, void *context) {
-  //print on screen
-  
+  app_message_outbox_begin(&iter);
+  dict_write_uint8(iter, 0x1, 1);
+  app_message_outbox_send();
+  APP_LOG(APP_LOG_LEVEL_DEBUG,"Msg Sent!");
+}
+void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+  //send alert to phone via bluetooth
+  send_alert_to_phone();
+}
+
+void select_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
+  //recv ACK from phone and print
+
 }
 
 int16_t max(int16_t a_in, int16_t b_in, int16_t c_in ){
@@ -27,32 +40,59 @@ int16_t max(int16_t a_in, int16_t b_in, int16_t c_in ){
     if(b>c)return b;
     else return c;
   }
-} 
+}
 
 bool isFalling(AccelData *data) {
-  if (max(data[0].x, data[0].y, data[0].z)>THRES){
+  if (max(data[0].x, data[0].y, data[0].z)<THRES){
     return true;
   }
-  
+
   return false;
 }
+/*
+static void data_handler(AccelData *data, uint32_t num_samples) {
+// Long lived buffer
+static char s_buffer[128];
+if (isFalling(data)) {
+// Compose string of all data
+snprintf(s_buffer, sizeof(s_buffer),
+"X,Y,Z\n %d,%d,%d",
+data[0].x, data[0].y, data[0].z
+);
+
+//Show the data
+text_layer_set_text(s_output_layer, s_buffer);
+} else {
+text_layer_set_text(s_output_layer, "Not falling\n");
+}
+//memset(s_buffer, 0, sizeof(s_buffer));
+}
+*/
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
   // Long lived buffer
   static char s_buffer[128];
-  if (isFalling(data)) {
-    // Compose string of all data
-    snprintf(s_buffer, sizeof(s_buffer), 
-	     "X,Y,Z\n %d,%d,%d", 
-	     data[0].x, data[0].y, data[0].z
-	     );
-  
-    //Show the data
-    text_layer_set_text(s_output_layer, s_buffer);
-  } else {
-    text_layer_set_text(s_output_layer, "Not falling\n");
+
+  // Compose string of all data for 3 samples
+  // double g=sqrt(static_cast<double>(data[0].x*data[0].x+data[0].y*data[0].y+data[0].z*data[0].z));
+  snprintf(s_buffer, sizeof(s_buffer),
+	   "N X,Y,Z\n0 %d,%d,%d\n1 %d,%d,%d\n2 %d,%d,%d\n",
+	   data[0].x, data[0].y, data[0].z,
+	   data[1].x, data[1].y, data[1].z,
+	   data[2].x, data[2].y, data[2].z
+	   );
+
+
+  // APP_LOG(APP_LOG_LEVEL_INFO, "time=%d, X=%d, Y=%d, Z=%d ",(int)data[0].timestamp, data[0].x, data[0].y, data[0].z);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"force=%f", g);
+
+  //Show the data
+  text_layer_set_text(s_output_layer, s_buffer);
+
+  if(isFalling(data)){
+    send_alert_to_phone();
   }
-  memset(s_buffer, 0, sizeof(s_buffer));
+
 }
 
 static void main_window_load(Window *window) {
@@ -72,27 +112,37 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_output_layer);
 }
 
+void config_provider(Window * window){
+  window_long_click_subscribe(BUTTON_ID_SELECT, 700, select_long_click_handler, select_long_click_release_handler);
+}
+
 static void init() {
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
   // Create main Window
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
       .load = main_window_load,
-    .unload = main_window_unload
+.unload = main_window_unload
 	});
   window_stack_push(s_main_window, true);
 
   // Subscribe to the accelerometer data service
-  int num_samples = 1;
+  int num_samples = 3;
   accel_data_service_subscribe(num_samples, data_handler);
 
   // Choose update rate
-  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
+
+  window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
+
 }
+
 
 static void deinit() {
   // Destroy main Window
   window_destroy(s_main_window);
   accel_data_service_unsubscribe();
+
 }
 
 int main(void) {
@@ -100,3 +150,4 @@ int main(void) {
   app_event_loop();
   deinit();
 }
+
