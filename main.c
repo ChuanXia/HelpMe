be45a058a7636007a68ffca61a08c2489f8b1a09
claireyuan92 <meng.yuan@duke.edu>
2015-03-22 12:28:12 -0400

    edited

--- main.c
+++ main.c
@@ -15,6 +15,9 @@ static TextLayer *alert_layer;
 static TextLayer *select_layer;
 static bool has_select_window;
 
+static int s_uptime = 0;
+
+
 
 int16_t max(int16_t a_in, int16_t b_in, int16_t c_in ){
   int16_t a = abs(a_in);
@@ -66,26 +69,6 @@ static void alert_window_unload(Window *window) {
   text_layer_destroy(alert_layer);
 }
 
-//Select window loading
-static void select_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
-
-  // Create output TextLayer
-  select_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
-  text_layer_set_font(select_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(select_layer, "Do you want to send the alert? UP:YES | DOWN:NO");
-  text_layer_set_overflow_mode(select_layer, GTextOverflowModeWordWrap);
-  layer_add_child(window_layer, text_layer_get_layer(select_layer));
-
-}
-
-static void select_window_unload(Window *window) {
-  // Destroy output TextLayer
-  text_layer_destroy(select_layer);
-}
-
-
 void send_alert_to_phone(){
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
@@ -105,17 +88,50 @@ void send_alert_to_phone(){
  
 }
 
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  if(s_uptime==10){
+    send_alert_to_phone();  
+  }
+  s_uptime++;
+}
+//Select window loading
+static void select_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  // Create output TextLayer
+  select_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
+  text_layer_set_font(select_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(select_layer, "Do you want to send the alert? UP:YES | DOWN:NO");
+  
+
+  
+  text_layer_set_overflow_mode(select_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(select_layer));
+
+}
+
+static void select_window_unload(Window *window) {
+  // Destroy output TextLayer
+  text_layer_destroy(select_layer);
+}
+
+
+
+
 void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
   //send alert to phone via bluetooth
   // accel_data_service_unsubscribe();
   if(click_recognizer_get_button_id(recognizer)==BUTTON_ID_UP){
     send_alert_to_phone();
   }
-  /*
-  else{
-    window_destroy(select_window);
+  
+  else if(click_recognizer_get_button_id(recognizer)==BUTTON_ID_DOWN){
+    s_uptime=100;
+    
   }
-  */
+
   
 }
 void select_long_click_handler(ClickRecognizerRef recognizer, void *context){
@@ -143,11 +159,12 @@ bool isFalling(AccelData *data) {
 
 
 void config_provider(Window * window){
+  
   window_single_click_subscribe(BUTTON_ID_UP,select_single_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN,select_single_click_handler);
   window_long_click_subscribe(BUTTON_ID_SELECT, 700, select_long_click_handler, select_long_click_release_handler);
 }
-
+//Whether to send signal after fall down
 void create_select_dialogue(){
   if (!has_select_window) {
     select_window = window_create();
@@ -157,8 +174,9 @@ void create_select_dialogue(){
         .load = select_window_load,
       .unload = select_window_unload
 	  });
-      
     window_stack_push(select_window, true);
+    APP_LOG(APP_LOG_LEVEL_DEBUG,"%d", s_uptime);
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
     window_set_click_config_provider(select_window, (ClickConfigProvider) config_provider);
   } else {
     // set select_window as the active window
@@ -256,7 +274,7 @@ static void init() {
   accel_data_service_subscribe(num_samples, data_handler);
 
   // Choose update rate
-  accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
 
   window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
 
